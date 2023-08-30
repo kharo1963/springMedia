940ea760151bfba2fd05594022b409426ce98179
kharo1963 <kharo1963@gmail.com>
2023-08-30 11:56:32 +0300

    settings

--- src/main/java/com/mediaAPI/MediaAPIApplication.java
+++ src/main/java/com/mediaAPI/MediaAPIApplication.java
@@ -1,14 +1,7 @@
 package com.mediaAPI;
 
-import com.mediaAPI.auth.AuthenticationService;
-import com.mediaAPI.auth.RegisterRequest;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import static com.mediaAPI.user.Role.ADMIN;
-import static com.mediaAPI.user.Role.MANAGER;
 
 @SpringBootApplication
 public class MediaAPIApplication {
